b075afe953468308759416010d927f7ccf99f745
Ugochukwu Umerie <umerieugochukwu@gmail.com>
2024-04-16 13:41:24 +0100

    feat: Add banner section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,15 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import localFont from "next/font/local"
 import "../shared/styles/globals.css";
 import Providers from "@/shared/utils/providers";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
+const clashDisplay = localFont({
+  src: "../assets/fonts/ClashDisplay-Variable.ttf",
+  variable: "--font-clashDisplay",
+  weight: "700"
+})
 
 export const metadata: Metadata = {
   title: "Newsletter App",
@@ -17,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={clashDisplay.variable}>
         <Providers>{children}</Providers>
       </body>
     </html>
--- src/modules/home/home.tsx
+++ src/modules/home/home.tsx
@@ -1,9 +1,11 @@
 import Header from "@/shared/widgets/header/header";
+import Banner from "./features/banner";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Banner />
     </div>
   );
 };
