06ddc574ed80a54a9e4cb56fa3544610401fa0a3
Ugochukwu Umerie <umerieugochukwu@gmail.com>
2024-04-17 17:09:14 +0100

    feat: Add branding, benefits and feature.highlight components

--- src/modules/home/home.tsx
+++ src/modules/home/home.tsx
@@ -1,11 +1,17 @@
 import Header from "@/shared/widgets/header/header";
 import Banner from "./features/banner";
+import Branding from "./features/branding";
+import Benefits from "./features/benefits";
+import FeatureHighlight from "./features/feature.highlight";
 
 const Home = () => {
   return (
     <div>
       <Header />
       <Banner />
+      <Branding />
+      <Benefits />
+      <FeatureHighlight />
     </div>
   );
 };
